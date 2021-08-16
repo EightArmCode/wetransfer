cb58bd8cccecc9e99cb82bc771f4afc3acfc7eb9
Chilton <kat@eightarmcode.com>
2021-08-16 22:16:25 +0200

    still fiddling with missing types! Dear heroku...

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import { join } from 'path'
 
 class App {
     
@@ -8,14 +7,10 @@ class App {
     this.express = express()
     this.mountRoutes()
   }
-  private mountRoutes(): void {
+  private mountRoutes():void {
     const router = express.Router()
-    // router.get('/', (req:Request, res:Response) => {
-    //   res.send({ message: 'Go away, world!' })
-    // })
     this.express.use('/', router)
     this.express.use(express.static('public'))
-    // this.express.use('/public', express.static(join('public')))
   }
 }
 export default new App().express
